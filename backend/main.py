68094bac998afc4123e956fa58f5f7a1056c0720
jmcclay4 <jamesjmcclay@gmail.com>
2025-06-24 11:27:13 -0500

    Add wildcard CORS for debugging

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://oversold.jmcclay.com"],
+    allow_origins=["http://localhost:5173", "https://oversold.jmcclay.com", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
